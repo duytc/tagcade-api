bd4dbe3c32bb517c61d611e843c66de1fac1538f
litpuvn <litpuvn@gmail.com>
2014-12-09 15:44:58 +0700

    remove namespace cache for now and we will reuse it latter

--- src/Tagcade/Legacy/Cache/TagCache.php
+++ src/Tagcade/Legacy/Cache/TagCache.php
@@ -2,12 +2,10 @@
 
 namespace Tagcade\Legacy;
 
-use Doctrine\Common\Cache\Cache;
 use Tagcade\DomainManager\AdSlotManagerInterface;
 use Tagcade\DomainManager\AdTagManagerInterface;
 use Tagcade\Legacy\Cache\RedisArrayCache;
 use Tagcade\Model\Core\AdSlotInterface;
-use Tagcade\Model\Core\AdTagInterface;
 
 class TagCache implements TagCacheInterface
 {
@@ -20,81 +18,78 @@ class TagCache implements TagCacheInterface
      */
     private $adTagManager;
     /**
-     * @var NamespaceCacheInterface
-     */
-    private $namespaceCache;
-    /**
      * @var RedisArrayCache
      */
     private $cache;
 
-    function __construct(AdSlotManagerInterface $adSlotManager, AdTagManagerInterface $adTagManager, NamespaceCacheInterface $namespaceCache, RedisArrayCache $cache)
+    function __construct(AdSlotManagerInterface $adSlotManager, AdTagManagerInterface $adTagManager, RedisArrayCache $cache)
     {
         $this->adSlotManager = $adSlotManager;
         $this->adTagManager = $adTagManager;
-        $this->namespaceCache = $namespaceCache;
         $this->cache = $cache;
     }
 
     public function renewCacheForAdSlot(AdSlotInterface $adSlotId)
     {
-        // TODO: renew cache for one ad slot
-        // Step 1. Clear previous data
-
-        // Step 2. Generate new cache key
-        $newKey = 'all_tags_array'; //all_tags_array
-        // Step 3. Store new data with new cache key
-        $this->saveCacheAtKey($newKey, $adSlotId);
+        return $this;
+//        // TODO: renew cache for one ad slot
+//        // Step 1. Clear previous data
+//
+//        // Step 2. Generate new cache key
+//        $newKey = 'all_tags_array'; //all_tags_array
+//        // Step 3. Store new data with new cache key
+//        $this->saveCacheAtKey($newKey, $adSlotId);
     }
 
     public function renewCache()
     {
-        $adSlots = $this->adSlotManager->all();
-
-        foreach ($adSlots as $adSlot) {
-            $this->renewCacheForAdSlot($adSlot);
-        }
+        return $this;
+//        $adSlots = $this->adSlotManager->all();
+//
+//        foreach ($adSlots as $adSlot) {
+//            $this->renewCacheForAdSlot($adSlot);
+//        }
     }
 
-    /**
-     * @param $cacheKey
-     * @param AdSlotInterface $adSlot
-     * @return bool
-     */
-    protected function saveCacheAtKey($cacheKey, AdSlotInterface $adSlot)
-    {
-        $tagArray = $this->createCacheDataForAdSlot($adSlot);
-
-        return $this->cache->save($cacheKey, $tagArray);
-    }
-
-    /**
-     * @param AdSlotInterface $adSlot
-     * @return array
-     */
-    protected function createCacheDataForAdSlot(AdSlotInterface $adSlot)
-    {
-        $adTags = $this->adTagManager->getAdTagsForAdSlot($adSlot);
-
-        $tagArray = [];
-
-        /**
-         * @var AdTagInterface $tag
-         */
-
-        foreach ($adTags as $tag) {
-            $tempTagData = ['id'  => $tag->getId(), 'tag' => $tag->getTag()];
-
-            if (null !== $tag->getBlockSuspiciousTraffic()) {
-                $tempTagData['bst'] = (bool) $tag->getBlockSuspiciousTraffic();
-            }
-
-            $tagArray[] = $tempTagData;
-
-            unset($tempTagData);
-        }
-
-        return $tagArray;
-    }
+//    /**
+//     * @param $cacheKey
+//     * @param AdSlotInterface $adSlot
+//     * @return bool
+//     */
+//    protected function saveCacheAtKey($cacheKey, AdSlotInterface $adSlot)
+//    {
+//        $tagArray = $this->createCacheDataForAdSlot($adSlot);
+//
+//        return $this->cache->save($cacheKey, $tagArray);
+//    }
+//
+//    /**
+//     * @param AdSlotInterface $adSlot
+//     * @return array
+//     */
+//    protected function createCacheDataForAdSlot(AdSlotInterface $adSlot)
+//    {
+//        $adTags = $this->adTagManager->getAdTagsForAdSlot($adSlot);
+//
+//        $tagArray = [];
+//
+//        /**
+//         * @var AdTagInterface $tag
+//         */
+//
+//        foreach ($adTags as $tag) {
+//            $tempTagData = ['id'  => $tag->getId(), 'tag' => $tag->getTag()];
+//
+//            if (null !== $tag->getBlockSuspiciousTraffic()) {
+//                $tempTagData['bst'] = (bool) $tag->getBlockSuspiciousTraffic();
+//            }
+//
+//            $tagArray[] = $tempTagData;
+//
+//            unset($tempTagData);
+//        }
+//
+//        return $tagArray;
+//    }
 
 } 
