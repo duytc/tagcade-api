a990a38fb512a340679ff2fd339e1b8c3390b198
litpuvn <litpuvn@gmail.com>
2014-12-11 10:14:46 +0700

    a stub code to implement projected billing test

---
+++ src/Tagcade/Tests/Service/Report/PerformanceReport/Display/Billing/ProjectedBillingCalculatorTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tagcade\Tests\Service\Report\PerformanceReport\Display\Billing;
+
+class ProjectedBillingCalculatorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testProjectedBilledAmountForPublisher()
+    {
+
+    }
+} 
