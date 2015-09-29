cb7caec07171811d50234fef2be6f6604bac72ea
dunght160387 <dunght1603@gmail.com>
2015-09-29 17:07:30 +0700

    add doctrine migration version for AutoFit_For_Cache

---
+++ app/migrations/Version20150929152100_AutoFit_For_Cache.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tagcade\Migration;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ * Add Auto fit property to Tag cache
+ */
+class Version20150929152100_AutoFit_For_Cache extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE library_display_ad_slot ADD auto_fit TINYINT(1) DEFAULT \'0\' NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE library_display_ad_slot DROP auto_fit');
+    }
+}
