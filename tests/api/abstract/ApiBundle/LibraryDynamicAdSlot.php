9d772bba475083d200a62ce1fb6fcd12acd64b6d
Giang Coffee <giang.fet.hut@gmail.com>
2015-10-13 10:08:55 +0700

    add test case of switching library native ad slots in dynamic expression

--- tests/api/abstract/ApiBundle/LibraryDynamicAdSlot.php
+++ tests/api/abstract/ApiBundle/LibraryDynamicAdSlot.php
@@ -904,6 +904,50 @@ class LibraryDynamicAdSlot
         $I->seeResponseCodeIs(400);
     }
 
+    public function patchLibraryDynamicAdSlotWithNewLibraryNativeAdSlot(ApiTester $I)
+    {
+        $jsonData =  [
+            'name' => 'dtag.test.librarynativeadslot',
+        ];
+
+        $I->sendPOST(URL_API . '/librarynativeadslots', $jsonData);
+
+        $I->seeResponseCodeIs(201);
+        $I->sendGet(URL_API . '/librarynativeadslots');
+        $item = array_pop($I->grabDataFromJsonResponse());
+
+        $jsonData = self::$JSON_DATA_SAMPLE_LIBRARY_DYNAMIC_AD_SLOT;
+        unset($jsonData['name']);
+        unset($jsonData['defaultLibraryAdSlot']);
+        $jsonData['libraryExpressions'][0]['expectLibraryAdSlot'] = $item['id'];
+        unset($jsonData['libraryExpressions'][0]['startingPosition']);
+
+        //add new before editing
+        $jsonDynamic = self::$JSON_DATA_SAMPLE_LIBRARY_DYNAMIC_AD_SLOT;
+
+        // full expressionDescriptor
+        $jsonDynamic['libraryExpressions'][0]['expressionDescriptor'] = [
+            'groupType' => 'AND',
+            'groupVal' => [
+                [
+                    'var' => 'checkLength',
+                    'cmp' => 'length >=',
+                    'val' => 10,
+                    'type' => 'numeric'
+                ]
+            ]
+        ];
+        $jsonDynamic['native'] = true;
+        $I->sendPOST(URL_API . '/librarydynamicadslots', $jsonDynamic);
+        $I->seeResponseCodeIs(201);
+
+        $I->sendGet(URL_API . '/librarydynamicadslots');
+        $item = array_pop($I->grabDataFromJsonResponse());
+
+        $I->sendPATCH(URL_API . '/librarydynamicadslots/' . $item['id'], $jsonData);
+        $I->seeResponseCodeIs(204);
+    }
+
     /**
      * delete libraryDynamicAdSlot
      */
--- tests/api/admin/ApiBundle/LibraryDynamicAdSlotAdminCest.php
+++ tests/api/admin/ApiBundle/LibraryDynamicAdSlotAdminCest.php
@@ -33,4 +33,50 @@ class LibraryDynamicAdSlotAdminCest extends LibraryDynamicAdSlot
             'defaultLibraryAdSlot' => PARAMS_LIBRARY_AD_SLOT
         ];
     }
+
+    public function patchLibraryDynamicAdSlotWithNewLibraryNativeAdSlot(ApiTester $I)
+    {
+        $jsonData =  [
+            'name' => 'dtag.test.librarynativeadslot',
+            'publisher' => PARAMS_PUBLISHER
+        ];
+
+        $I->sendPOST(URL_API . '/librarynativeadslots', $jsonData);
+
+        $I->seeResponseCodeIs(201);
+        $I->sendGet(URL_API . '/librarynativeadslots');
+        $item = array_pop($I->grabDataFromJsonResponse());
+
+        $jsonData = self::$JSON_DATA_SAMPLE_LIBRARY_DYNAMIC_AD_SLOT;
+        unset($jsonData['name']);
+        unset($jsonData['defaultLibraryAdSlot']);
+        $jsonData['libraryExpressions'][0]['expectLibraryAdSlot'] = $item['id'];
+        unset($jsonData['libraryExpressions'][0]['startingPosition']);
+
+        //add new before editing
+        $jsonDynamic = self::$JSON_DATA_SAMPLE_LIBRARY_DYNAMIC_AD_SLOT;
+
+        // full expressionDescriptor
+        $jsonDynamic['libraryExpressions'][0]['expressionDescriptor'] = [
+            'groupType' => 'AND',
+            'groupVal' => [
+                [
+                    'var' => 'checkLength',
+                    'cmp' => 'length >=',
+                    'val' => 10,
+                    'type' => 'numeric'
+                ]
+            ]
+        ];
+        $jsonDynamic['native'] = true;
+        $jsonDynamic['publisher'] = PARAMS_PUBLISHER;
+        $I->sendPOST(URL_API . '/librarydynamicadslots', $jsonDynamic);
+        $I->seeResponseCodeIs(201);
+
+        $I->sendGet(URL_API . '/librarydynamicadslots');
+        $item = array_pop($I->grabDataFromJsonResponse());
+
+        $I->sendPATCH(URL_API . '/librarydynamicadslots/' . $item['id'], $jsonData);
+        $I->seeResponseCodeIs(204);
+    }
 }
