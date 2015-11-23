cbe3e61b18d56fe8c078209f28a1de2cb63b242b
Giang Coffee <giang.fet.hut@gmail.com>
2015-11-23 08:46:03 +0700

    update AdTag count when changing AdNetwork

--- src/Tagcade/Bundle/ApiBundle/EventListener/UpdateAdTagCountForAdNetworkListener.php
+++ src/Tagcade/Bundle/ApiBundle/EventListener/UpdateAdTagCountForAdNetworkListener.php
@@ -5,15 +5,18 @@ namespace Tagcade\Bundle\ApiBundle\EventListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Tagcade\Model\Core\AdNetworkInterface;
 use Tagcade\Model\Core\AdTagInterface;
+use Tagcade\Model\Core\LibraryAdTagInterface;
 
 class UpdateAdTagCountForAdNetworkListener
 {
     const AD_TAG_UPDATE = 1;
     const AD_TAG_PERSIST = 2;
     const AD_TAG_DELETE = 3;
-
+    private $changedAdNetworks = [];
     /**
      * handle event prePersist to detect new ad tag is added, used for updating number of active|paused ad tags of ad network
      * @param LifecycleEventArgs $args
@@ -29,6 +32,42 @@ class UpdateAdTagCountForAdNetworkListener
 
         $this->increaseActiveAdTagCountForAdNetwork($entity, self::AD_TAG_PERSIST);
     }
+
+    public function preUpdate(PreUpdateEventArgs $args)
+    {
+        $entity = $args->getObject();
+
+        if ($entity instanceof LibraryAdTagInterface && $args->hasChangedField('adNetwork')) {
+            $adTags = $entity->getAdTags();
+            /** @var AdNetworkInterface $oldAdNetwork, $newAdNetwork */
+            $oldAdNetwork = $args->getOldValue('adNetwork');
+            /** @var AdNetworkInterface $newAdNetwork */
+            $newAdNetwork = $args->getNewValue('adNetwork');
+            /** @var AdTagInterface $adTag */
+            foreach($adTags as $adTag) {
+                $adTag->isActive() ? $oldAdNetwork->decreaseActiveAdTagsCount() : $oldAdNetwork->decreasePausedAdTagsCount();
+                $adTag->isActive() ? $newAdNetwork->increaseActiveAdTagsCount() : $newAdNetwork->increasePausedAdTagsCount();
+                $this->changedAdNetworks[] = $oldAdNetwork;
+                $this->changedAdNetworks[] = $newAdNetwork;
+            }
+        }
+    }
+
+    public function postFlush(PostFlushEventArgs $args)
+    {
+        if(count($this->changedAdNetworks) < 1) {
+            return;
+        }
+
+        $em = $args->getEntityManager();
+        foreach($this->changedAdNetworks as $adNetwork) {
+            $em->merge($adNetwork);
+        }
+        $this->changedAdNetworks = [];
+        $em->flush();
+
+    }
+
 
     public function onFlush(OnFlushEventArgs $eventArgs)
     {
@@ -48,6 +87,7 @@ class UpdateAdTagCountForAdNetworkListener
             $uow->recomputeSingleEntityChangeSet($meta, $entity->getAdNetwork());
         }
     }
+
     /**
      * handle event prePersist to detect new ad tag is added, used for updating number of active|paused ad tags of ad network
      * @param LifecycleEventArgs $args
