f9eb57f1f5efc2e72087f389a8b1e05fff9a7c82
dunght160387 <dunght1603@gmail.com>
2015-11-30 18:22:37 +0700

    Using AdNetworkPartner id in service url instead of name

--- src/Tagcade/Bundle/ReportApiBundle/Controller/UnifiedReportController.php
+++ src/Tagcade/Bundle/ReportApiBundle/Controller/UnifiedReportController.php
@@ -7,10 +7,13 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Tagcade\Entity\Core\AdNetworkPartner;
+use Tagcade\Exception\NotSupportedException;
 use Tagcade\Model\Report\UnifiedReport\ReportType\PulsePoint\AccountManagement as AccountManagementReportType;
 use Tagcade\Model\Report\UnifiedReport\ReportType\PulsePoint\Daily as DailyReportType;
 use Tagcade\Model\Report\UnifiedReport\ReportType\PulsePoint\DomainImpression as DomainImpressionReportType;
 use Tagcade\Model\Report\UnifiedReport\ReportType\PulsePoint\CountryDaily as CountryDailyReportType;
+use Tagcade\Model\Report\UnifiedReport\ReportType\ReportTypeInterface;
 use Tagcade\Model\User\Role\AdminInterface;
 use Tagcade\Model\User\Role\PublisherInterface;
 use Tagcade\Service\Report\UnifiedReport\Selector\UnifiedReportParams;
@@ -22,23 +25,45 @@ use Tagcade\Service\Report\UnifiedReport\Selector\UnifiedReportParams;
  */
 class UnifiedReportController extends FOSRestController
 {
+    static $REPORT_TYPE_MAP = [
+        'adtag' => ['Pulse Point'],
+        'daily' => ['Pulse Point'],
+        'site' => ['Pulse Point'],
+        'country' => ['Pulse Point']
+    ];
+
     /**
      * @Security("has_role('ROLE_ADMIN') or has_role('ROLE_PUBLISHER')")
      *
-     * @Rest\Get("/adtag")
+     * @Rest\Get("/{id}", requirements={"id" = "\d+"})
      *
      * @Rest\QueryParam(name="publisher", requirements="\d+", nullable=true)
      * @Rest\QueryParam(name="startDate", requirements="\d{4}-\d{2}-\d{2}", nullable=true)
      * @Rest\QueryParam(name="endDate", requirements="\d{4}-\d{2}-\d{2}", nullable=true)
+     * @Rest\QueryParam(name="breakDown", requirements="\s", nullable=true)
      * @Rest\QueryParam(name="group", requirements="(true|false)", nullable=true)
      *
+     * @param $id
      * @param Request $request
+     * @throws NotSupportedException
      * @return array
      */
-    public function getAccountManagementReportAction(Request $request)
+    public function getPartnerReportsAction($id, Request $request)
     {
-        $service = $this->get('tagcade.service.report.unified_report.selector.report_selector');
+        /* validate adNetworkPartner by $id */
+        $adNetworkPartner = $this->get('tagcade.repository.ad_network_partner')->find($id);
 
+        if(!$adNetworkPartner instanceof AdNetworkPartner) {
+            throw new NotFoundHttpException('Not found that AdNetwork Partner');
+        }
+
+        /* validate breakDown */
+        $breakDown = $request->query->get('breakDown', null);
+        if (!$this->isSupportedReportType($adNetworkPartner, $breakDown)) {
+            throw new NotSupportedException('Not support that breakDown as ' . $breakDown);
+        }
+
+        /* validate publisherId */
         $user = $this->getUser();
 
         $publisher = $user;
@@ -52,104 +77,88 @@ class UnifiedReportController extends FOSRestController
             }
         }
 
-        return $this->getResult($service->getReports(new AccountManagementReportType($publisher, $date = new \DateTime()), $this->getParams()));
+        return $this->getReports($request, $publisher, $breakDown);
     }
 
     /**
-     * @Security("has_role('ROLE_ADMIN') or has_role('ROLE_PUBLISHER')")
-     *
-     * @Rest\Get("/daily")
-     *
-     * @Rest\QueryParam(name="publisher", requirements="\d+", nullable=true)
-     * @Rest\QueryParam(name="startDate", requirements="\d{4}-\d{2}-\d{2}", nullable=true)
-     * @Rest\QueryParam(name="endDate", requirements="\d{4}-\d{2}-\d{2}", nullable=true)
-     * @Rest\QueryParam(name="group", requirements="(true|false)", nullable=true)
-     *
-     * @param Request $request
-     * @return array
+     * check if supported breakDown
+     * @param AdNetworkPartner $adNetworkPartner
+     * @param $breakDown
+     * @return bool|ReportTypeInterface
      */
-    public function getDailyReportAction(Request $request)
+    private function isSupportedReportType(AdNetworkPartner $adNetworkPartner, $breakDown)
+    {
+        if(!$adNetworkPartner instanceof AdNetworkPartner
+            || !array_key_exists($breakDown, self::$REPORT_TYPE_MAP)
+            || self::$REPORT_TYPE_MAP[$breakDown] !== $adNetworkPartner->getName()
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * get Reports by publisher and breakDown
+     * @param $publisher
+     * @param $breakDown
+     * @throws NotSupportedException
+     */
+    private function getReports($publisher, $breakDown)
+    {
+        if ('adtag' === $breakDown) {
+            return $this->getAccountManagementReportAction($publisher);
+        } elseif ('daily' === $breakDown) {
+            return $this->getDailyReportAction($publisher);
+        } elseif ('site' === $breakDown) {
+            return $this->getDomainImpressionReportAction($publisher);
+        } elseif ('country' === $breakDown) {
+            return $this->getCountryDailyReportAction($publisher);
+        }
+
+        throw new NotSupportedException('Not support that breakDown as ' . $breakDown);
+    }
+
+    /**
+     * @param PublisherInterface $publisher
+     * @return mixed
+     */
+    private function getAccountManagementReportAction(PublisherInterface $publisher)
     {
         $service = $this->get('tagcade.service.report.unified_report.selector.report_selector');
 
-        $user = $this->getUser();
+        return $this->getResult($service->getReports(new AccountManagementReportType($publisher, $tagId = null), $this->getParams()));
+    }
 
-        $publisher = $user;
-
-        if ($this->getUser() instanceof AdminInterface) {
-            $publisherId = $request->query->get('publisher', null);
-            $publisher = $this->get('tagcade_user.domain_manager.publisher')->find($publisherId);
-
-            if (!$publisher instanceof PublisherInterface) {
-                throw new NotFoundHttpException('Not found that publisher');
-            }
-        }
+    /**
+     * @param PublisherInterface $publisher
+     * @return mixed
+     */
+    private function getDailyReportAction(PublisherInterface $publisher)
+    {
+        $service = $this->get('tagcade.service.report.unified_report.selector.report_selector');
 
         return $this->getResult($service->getReports(new DailyReportType($publisher, $date = new \DateTime()), $this->getParams()));
     }
 
     /**
-     * @Security("has_role('ROLE_ADMIN') or has_role('ROLE_PUBLISHER')")
-     *
-     * @Rest\Get("/site")
-     *
-     * @Rest\QueryParam(name="publisher", requirements="\d+", nullable=true)
-     * @Rest\QueryParam(name="startDate", requirements="\d{4}-\d{2}-\d{2}", nullable=true)
-     * @Rest\QueryParam(name="endDate", requirements="\d{4}-\d{2}-\d{2}", nullable=true)
-     * @Rest\QueryParam(name="group", requirements="(true|false)", nullable=true)
-     *
-     * @param Request $request
-     * @return array
+     * @param PublisherInterface $publisher
+     * @return mixed
      */
-    public function getDomainImpressionReportAction(Request $request)
+    private function getDomainImpressionReportAction(PublisherInterface $publisher)
     {
         $service = $this->get('tagcade.service.report.unified_report.selector.report_selector');
-
-        $user = $this->getUser();
-
-        $publisher = $user;
-
-        if ($this->getUser() instanceof AdminInterface) {
-            $publisherId = $request->query->get('publisher', null);
-            $publisher = $this->get('tagcade_user.domain_manager.publisher')->find($publisherId);
-
-            if (!$publisher instanceof PublisherInterface) {
-                throw new NotFoundHttpException('Not found that publisher');
-            }
-        }
 
         return $this->getResult($service->getReports(new DomainImpressionReportType($publisher, $date = new \DateTime()), $this->getParams()));
     }
 
     /**
-     * @Security("has_role('ROLE_ADMIN') or has_role('ROLE_PUBLISHER')")
-     *
-     * @Rest\Get("/country")
-     *
-     * @Rest\QueryParam(name="publisher", requirements="\d+", nullable=true)
-     * @Rest\QueryParam(name="startDate", requirements="\d{4}-\d{2}-\d{2}", nullable=true)
-     * @Rest\QueryParam(name="endDate", requirements="\d{4}-\d{2}-\d{2}", nullable=true)
-     * @Rest\QueryParam(name="group", requirements="(true|false)", nullable=true)
-     *
-     * @param Request $request
-     * @return array
+     * @param PublisherInterface $publisher
+     * @return mixed
      */
-    public function getCountryDailyReportAction(Request $request)
+    private function getCountryDailyReportAction(PublisherInterface $publisher)
     {
         $service = $this->get('tagcade.service.report.unified_report.selector.report_selector');
-
-        $user = $this->getUser();
-
-        $publisher = $user;
-
-        if ($this->getUser() instanceof AdminInterface) {
-            $publisherId = $request->query->get('publisher', null);
-            $publisher = $this->get('tagcade_user.domain_manager.publisher')->find($publisherId);
-
-            if (!$publisher instanceof PublisherInterface) {
-                throw new NotFoundHttpException('Not found that publisher');
-            }
-        }
 
         return $this->getResult($service->getReports(new CountryDailyReportType($publisher, $country = null, $tagId = null), $this->getParams()));
     }
