051dfd6255b31fdf77615368dbd12071f1487b40
Giang Coffee <giang.fet.hut@gmail.com>
2015-12-16 13:52:37 +0700

    add serializer for RonSlotReport

--- src/Tagcade/Entity/Report/PerformanceReport/Display/Segment/RonAdSlotReport.php
+++ src/Tagcade/Entity/Report/PerformanceReport/Display/Segment/RonAdSlotReport.php
@@ -5,4 +5,22 @@ namespace Tagcade\Entity\Report\PerformanceReport\Display\Segment;
 use Tagcade\Model\Report\PerformanceReport\Display\Hierarchy\Segment\RonAdSlotReport as RonAdSlotReportModel;
 
 class RonAdSlotReport extends RonAdSlotReportModel
-{}
+{
+    protected $id;
+    protected $superReport;
+    protected $subReports;
+    protected $name;
+    protected $date;
+    protected $totalOpportunities;
+    protected $impressions;
+    protected $passbacks;
+    protected $fillRate;
+    protected $estRevenue;
+    protected $estCpm;
+    protected $ronAdSlot;
+    protected $segment;
+    protected $customRate;
+    protected $slotOpportunities;
+    protected $billedAmount;
+    protected $billedRate;
+}
