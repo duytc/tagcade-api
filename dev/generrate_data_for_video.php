968b3bed6924688052d0d5ce299d84fa16393c6d
Giang Coffee <giang.fet.hut@gmail.com>
2016-09-26 14:18:48 +0700

    refactor generate video data script

--- dev/generrate_data_for_video.php
+++ dev/generrate_data_for_video.php
@@ -8,6 +8,15 @@ $kernel->boot();
 
 $container = $kernel->getContainer();
 
+use Tagcade\Bundle\UserBundle\DomainManager\PublisherManagerInterface;
+use Tagcade\Entity\Core\VideoDemandPartner;
+use Tagcade\Entity\Core\VideoPublisher;
+use Tagcade\Bundle\UserSystem\PublisherBundle\Entity\User;
+use Tagcade\Entity\Core\VideoWaterfallTag;
+use Tagcade\Entity\Core\LibraryVideoDemandAdTag;
+use Tagcade\Entity\Core\VideoWaterfallTagItem;
+use Tagcade\Entity\Core\VideoDemandAdTag;
+
 /** @var \Doctrine\ORM\EntityManagerInterface $em */
 $em = $container->get('doctrine.orm.entity_manager');
 $userManager = $container->get('tagcade_user.domain_manager.publisher');
@@ -16,122 +25,106 @@ $videoDemandPartnerManager = $container->get('tagcade.domain_manager.video_deman
 $videoDemandAdTagManager = $container->get('tagcade.domain_manager.video_demand_ad_tag');
 $videoAdTagManager = $container->get('tagcade.domain_manager.video_waterfall_tag');
 
-$dataNumber = [
-    'publisher' => 20,
-    'videoDemandPartner' => 20,
-    'videoPublisherForPublisher' => 20,
-    'videoWaterfallForVideoPublisher' => 20,
-    'videoDemandAdTagForWaterfall' => 20
-];
+/** @var PublisherManagerInterface $publisherManager */
+$publisherManager = $container->get('tagcade_user.domain_manager.publisher');
 
-$nameDefault = [
-    'publisher' => 'publisher',
-    'videoPublisher' => 'Video Publisher',
-    'videoDemandPartner' => 'Video Demand Partner',
-    'videoWaterfall' => 'Video Waterfall',
-    'videoDemandAdTag' => 'Video Demand Ad Tag'
-];
+const NUM_PUBLISHER = 2;
+const NUM_VIDEO_DEMAND_PARTNER = 3;
+const NUM_VIDEO_PUBLISHER = 5;
+const NUM_WATERFALL_TAG_PER_VIDEO_PUBLISHER = 20;
+const NUM_VIDEO_DEMAND_AD_TAG_PER_WATERFALL = 20;
+
+function xrange($max = 1000) {
+    for ($i = 1; $i <= $max; $i++) {
+        yield $i;
+    }
+}
 
 // create publisher
-for ($p = 1; $p <= $dataNumber['publisher']; $p++) {
+foreach(xrange(NUM_PUBLISHER) as $publisherId) {
     $demandPartners = [];
 
-    $publisher = new Tagcade\Bundle\UserSystem\PublisherBundle\Entity\User();
-    $username = $nameDefault['publisher'].$p; // set name for publisher
+    $publisher = new User();
+    $username = sprintf('tctest%d', $publisherId);
+    $publisher
+        ->setUsername($username)
+        ->setPlainPassword('123456')
+        ->setEmail(sprintf('tctest%d@tagcade.com', $publisherId))
+        ->setEnabled(true)
+    ;
 
-    $publisher->setUsername($username);
-    $publisher->setUsernameCanonical($username);
-    $publisher->setPassword($username);
-    $publisher->setEmail($username.'@gmail.com');
-    $publisher->setCompany($username);
+    $publisher->setCompany('tctest'); // doesn't return $this so cannot chain
     $publisher->setEnabledModules([$publisher::MODULE_DISPLAY, $publisher::MODULE_VIDEO]);
-    $publisher->setEnabled(true);
 
-    $em->persist($publisher);
-
-    echo sprintf('create publisher %s' . "\n", $username) ;
-
-    // create video demand partner
-    for ($vd = 1; $vd <= $dataNumber['videoDemandPartner']; $vd++) {
-        $videoDemandPartner = new Tagcade\Entity\Core\VideoDemandPartner();
-        $videoDemandPartnerName = $nameDefault['videoDemandPartner'].$vd . '-' . $username; // set name for video demand partner
-
-        $videoDemandPartner->setName($videoDemandPartnerName);
-        $videoDemandPartner->setPublisher($publisher);
-        $em->persist($videoDemandPartner);
-
-        $demandPartners[] = $videoDemandPartner;
-        echo sprintf('create video demand partner %s' . "\n", $videoDemandPartnerName);
-    }
+    $publisherManager->save($publisher);
+    $em->flush();
 
     // create video publisher
-    for ($vp = 1; $vp <= $dataNumber['videoPublisherForPublisher']; $vp++) {
+    foreach(xrange(NUM_VIDEO_PUBLISHER) as $videoPublisherId) {
         gc_enable();
-
         $tempObjs = [];
 
-        $videoPublisher = new Tagcade\Entity\Core\VideoPublisher();
-        $videoPublisherName = $nameDefault['videoPublisher'].$vp . '-' . $username; // set name for video publisher
+        $videoDemandPartner = (new VideoDemandPartner())
+            ->setName(sprintf('Video Demand Partner %d - %s', $videoPublisherId, $username))
+            ->setPublisher($publisher)
+        ;
 
-        $videoPublisher->setName($videoPublisherName);
-        $videoPublisher->setPublisher($publisher);
+        $em->persist($videoDemandPartner);
+        $tempObjs[] = $videoDemandPartner;
+
+        $videoPublisher =
+            (new VideoPublisher())
+            ->setName(sprintf('Video Publisher %d - %s', $videoPublisherId, $username))
+            ->setPublisher($publisher)
+        ;
 
         $em->persist($videoPublisher);
-
         $tempObjs[] = $videoPublisher;
 
-        echo sprintf('create video publisher %s' . "\n", $videoPublisherName) ;
-
         // create waterfall tag
-        for ($vw = 1; $vw <= $dataNumber['videoWaterfallForVideoPublisher']; $vw++) {
-            $videoWaterfall = new Tagcade\Entity\Core\VideoWaterfallTag();
-            $videoWaterfallName = $nameDefault['videoWaterfall'].$vw . '-' . $videoPublisherName; // set name for video waterfall tag
-
-            $videoWaterfall->setVideoPublisher($videoPublisher);
-            $videoWaterfall->setName($videoWaterfallName);
-            $videoWaterfall->setPlatform(['flash', 'js']);
-            $videoWaterfall->setUuid(generateUuidV4());
-            $videoWaterfall->setTargeting([]);
+        foreach(xrange(NUM_WATERFALL_TAG_PER_VIDEO_PUBLISHER) as $waterfallTag) {
+            $videoWaterfall = (new VideoWaterfallTag())
+                ->setVideoPublisher($videoPublisher)
+                ->setName(sprintf('Waterfall Tag %d - %s - %s', $waterfallTag, $videoPublisher->getName(), $username))
+                ->setPlatform(['flash', 'js'])
+                ->setUuid(generateUuidV4())
+                ->setTargeting([])
+            ;
 
             $em->persist($videoWaterfall);
             $tempObjs[] = $videoWaterfall;
 
-            echo sprintf('create video waterfall tag %s' . "\n", $videoWaterfallName);
-
             // create demand ad tag and demand partner
-            for ($vdt = 1; $vdt <= $dataNumber['videoDemandAdTagForWaterfall']; $vdt++) {
-                $videoDemandAdTag = new Tagcade\Entity\Core\VideoDemandAdTag();
-                $videoWaterfallTagItem = new Tagcade\Entity\Core\VideoWaterfallTagItem();
-                $videoDemandAdTagLibrary = new Tagcade\Entity\Core\LibraryVideoDemandAdTag();
+            foreach(xrange(NUM_VIDEO_DEMAND_AD_TAG_PER_WATERFALL) as $demandAdTag) {
 
-                $videoDemandAdTagName = $nameDefault['videoDemandAdTag'].$vdt . '-' . $videoWaterfallName; // set name for video demand ad tag
+                $videoWaterfallTagItem = (new VideoWaterfallTagItem())
+                    ->setStrategy('linear')
+                    ->setVideoWaterfallTag($videoWaterfall)
+                    ->setPosition($demandAdTag)
+                ;
 
-                // create library demand ad tag
-                $videoDemandAdTagLibrary->setName($videoDemandAdTagName);
-                $videoDemandAdTagLibrary->setVideoDemandPartner($demandPartners[rand(0, $dataNumber['videoDemandPartner'] -1)]);
-                $videoDemandAdTagLibrary->setTargeting([ 'countries' => [], 'exclude_countries' => [], 'domains' => [], 'exclude_domains' => [], 'platform' => [], 'player_size' => [], 'required_macros' => []]);
-                $videoDemandAdTagLibrary->setTimeout(3);
-                $videoDemandAdTagLibrary->setTagURL('https://search.spotxchange.com/vast/2?content_page_url=${page_url}&player_width=${player_width}');
-                $em->persist($videoDemandAdTagLibrary);
-                $tempObjs[] = $videoDemandAdTagLibrary;
-
-                echo sprintf('create library video demand ad tag %s' . "\n", $videoDemandAdTagName);
-
-                // create $videoWaterfallTagItem
-                $videoWaterfallTagItem->setStrategy('linear');
-                $videoWaterfallTagItem->setPosition($vdt-1);
-                $videoWaterfallTagItem->setVideoWaterfallTag($videoWaterfall);
-                $em->persist($videoWaterfallTagItem);
                 $tempObjs[] = $videoWaterfallTagItem;
 
-                // crete demand ad tag from library demand ad tag
-                $videoDemandAdTag->setLibraryVideoDemandAdTag($videoDemandAdTagLibrary);
-                $videoDemandAdTag->setTargetingOverride(false);
-                $videoDemandAdTag->setVideoWaterfallTagItem($videoWaterfallTagItem);
-                $em->persist($videoDemandAdTag);
-                $tempObjs[] = $videoDemandAdTag;
+                $videoDemandAdTagLibrary = (new LibraryVideoDemandAdTag())
+                ->setName(sprintf('Demand Ad Tag %d - %s - %s', $demandAdTag, $videoPublisher->getName(), $username))
+                ->setVideoDemandPartner($videoDemandPartner)
+                ->setTargeting([ 'countries' => [], 'exclude_countries' => [], 'domains' => [], 'exclude_domains' => [], 'platform' => [], 'player_size' => [], 'required_macros' => []])
+                ->setTimeout(3)
+                ->setTagURL('https://search.spotxchange.com/vast/2?content_page_url=${page_url}&player_width=${player_width}');
 
-                echo sprintf('create video demand ad tag %s' . "\n", $videoDemandAdTagName);
+                $videoDemandAdTag = (new VideoDemandAdTag())
+                    ->setLibraryVideoDemandAdTag($videoDemandAdTagLibrary)
+                    ->setTargetingOverride(false)
+                ;
+                $videoDemandAdTag->setVideoWaterfallTagItem($videoWaterfallTagItem);
+
+                $tempObjs[] = $videoDemandAdTag;
+                $tempObjs[] = $videoDemandAdTagLibrary;
+
+                $videoWaterfallTagItem->addVideoDemandAdTag($videoDemandAdTag);
+
+                $em->persist($videoWaterfallTagItem);
+                $tempObjs[] = $videoWaterfallTagItem;
             }
         }
 
@@ -142,12 +135,8 @@ for ($p = 1; $p <= $dataNumber['publisher']; $p++) {
         }
 
         gc_collect_cycles();
-    }
-
-    $em->flush();
-
-    foreach(array_reverse($demandPartners) as $obj) {
-        $em->detach($obj);
+        echo sprintf('finish inserting Video Publisher "%s"'. "\n", $videoPublisher->getName()) ;
+        unset($tempObjs);
     }
 
     $em->detach($publisher);
@@ -155,6 +144,7 @@ for ($p = 1; $p <= $dataNumber['publisher']; $p++) {
 }
 
 $em->flush();
+$em->clear();
 
 function generateUuidV4() {
     return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
