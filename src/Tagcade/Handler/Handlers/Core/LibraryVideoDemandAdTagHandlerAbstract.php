73d9be011193a858ce4c4b37deb77d75447bebca
Giang Coffee <giang.fet.hut@gmail.com>
2016-10-27 08:57:39 +0700

    update placement rule type manual

--- src/Tagcade/Handler/Handlers/Core/LibraryVideoDemandAdTagHandlerAbstract.php
+++ src/Tagcade/Handler/Handlers/Core/LibraryVideoDemandAdTagHandlerAbstract.php
@@ -32,7 +32,7 @@ abstract class LibraryVideoDemandAdTagHandlerAbstract extends RoleHandlerAbstrac
         $libraryVideoDemandAdTag = parent::post($parameters);
 
          // 2. create links to sites from this library ad slot
-        $this->getDomainManager()->deployLibraryVideoDemandAdTag($libraryVideoDemandAdTag);
+//        $this->getDomainManager()->deployLibraryVideoDemandAdTag($libraryVideoDemandAdTag);
 //        $this->getDomainManager()->deployLibraryVideoDemandAdTagBasedOnManualPlacementRule($libraryVideoDemandAdTag);
 
         return $libraryVideoDemandAdTag;
