c1653dc20d8f140d8b366b15b1b41b6cd340120a
Giang Coffee <giang.fet.hut@gmail.com>
2016-12-10 10:01:48 +0700

    update redis parameter name

--- dev/create_live_video_report_data.php
+++ dev/create_live_video_report_data.php
@@ -43,8 +43,8 @@ writeln('');
 writeln('   --> Start preparing redis cache');
 writeln('       ...');
 
-$host = $container->getParameter('tc.legacy.tag_cache.redis_host'); // or manually set value as tagcade.dev or localhost
-$port = $container->getParameter('tc.legacy.tag_cache.redis_port'); // or manually set value as 6379
+$host = $container->getParameter('tc.tag_cache.redis_host'); // or manually set value as tagcade.dev or localhost
+$port = $container->getParameter('tc.tag_cache.redis_port'); // or manually set value as 6379
 $redis = new Redis();
 $redis->connect($host, $port);
 $cache = new Tagcade\Cache\Legacy\Cache\RedisArrayCache();
