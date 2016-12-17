5344e4a5e7c112f380ecf2d771b8424a6ca12b48
Giang Coffee <giang.fet.hut@gmail.com>
2016-12-17 10:47:39 +0700

    refactor VerifySlotSynchronizationCommand

--- src/Tagcade/Bundle/AppBundle/Command/VerifySlotSynchronizationCommand.php
+++ src/Tagcade/Bundle/AppBundle/Command/VerifySlotSynchronizationCommand.php
@@ -9,16 +9,13 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Tagcade\Entity\Core\LibrarySlotTag;
+use Tagcade\Entity\Core\AdSlotAbstract;
+use Tagcade\Entity\Core\AdTag;
 use Tagcade\Exception\InvalidArgumentException;
 use Tagcade\Exception\RuntimeException;
-use Tagcade\Model\Core\AdTagInterface;
 use Tagcade\Model\Core\BaseAdSlotInterface;
 use Tagcade\Model\Core\LibrarySlotTagInterface;
 use Tagcade\Model\Core\RonAdSlotInterface;
-use Tagcade\Repository\Core\AdSlotRepositoryInterface;
-use Tagcade\Repository\Core\LibrarySlotTagRepositoryInterface;
-use Tagcade\Service\TagLibrary\ReplicatorInterface;
 
 class VerifySlotSynchronizationCommand extends ContainerAwareCommand
 {
@@ -35,7 +32,6 @@ class VerifySlotSynchronizationCommand extends ContainerAwareCommand
         $container = $this->getContainer();
         $em = $container->get('doctrine.orm.entity_manager');
         $ronSlotManager = $container->get('tagcade.domain_manager.ron_ad_slot');
-        $adSlotRepository = $container->get('tagcade.repository.ad_slot');
         $id = $input->getOption('id');
         $ronAdSlots = [];
         if ($id !== null) {
@@ -52,12 +48,14 @@ class VerifySlotSynchronizationCommand extends ContainerAwareCommand
          * @var RonAdSlotInterface $ronSlot
          */
         foreach($ronAdSlots as $ronSlot) {
-            $this->verifySingleRonSlot($em, $ronSlot, $adSlotRepository, $output);
+            $this->verifySingleRonSlot($em, $ronSlot, $output);
         }
     }
 
-    protected function verifySingleRonSlot(EntityManagerInterface $em, RonAdSlotInterface $ronSlot, AdSlotRepositoryInterface $adSlotRepository, OutputInterface $output)
+    protected function verifySingleRonSlot(EntityManagerInterface $em, RonAdSlotInterface $ronSlot, OutputInterface $output)
     {
+        $adSlotRepository = $em->getRepository(AdSlotAbstract::class);
+
         $libraryAdSlot = $ronSlot->getLibraryAdSlot();
         $coReferencedAdSlots = $adSlotRepository->getCoReferencedAdSlots($libraryAdSlot);
         if ($coReferencedAdSlots instanceof PersistentCollection) {
@@ -70,7 +68,7 @@ class VerifySlotSynchronizationCommand extends ContainerAwareCommand
          */
         foreach($coReferencedAdSlots as $slot) {
             if ($libraryAdSlot->checkSum() != $slot->checkSum()) {
-                $output->writeln(sprintf('<error>RON slot %d has one of its children out of sync</error>', $ronSlot->getId()));
+                $output->writeln(sprintf('<error>RON slot %d has its child, ad slot %d, out of sync</error>', $ronSlot->getId(), $slot->getId()));
 
                 $output->writeln(sprintf('<info>start fixing the bad ad slot %d</info>', $slot->getId()));
                 $this->fixSingleAdSlot($em, $slot);
@@ -86,36 +84,42 @@ class VerifySlotSynchronizationCommand extends ContainerAwareCommand
 
     protected function fixSingleAdSlot(EntityManagerInterface $em, BaseAdSlotInterface $adSlot)
     {
-        $librarySlotTagRepository = $em->getRepository(LibrarySlotTag::class);
-        $libSlotTags = $librarySlotTagRepository->getByLibraryAdSlot($adSlot->getLibraryAdSlot());
-        usort($libSlotTags, function(LibrarySlotTagInterface $a, LibrarySlotTagInterface $b) {
-            return strcmp($a->getRefId(), $b->getRefId());
-        });
+        $em->getConnection()->beginTransaction();
+        try {
+            $libAdSlot = $adSlot->getLibraryAdSlot();
+            $librarySlotTags = $libAdSlot->getLibSlotTags();
 
-        $adTags = $adSlot->getAdTags()->toArray();
-        usort($adTags, function(AdTagInterface $a, AdTagInterface $b) {
-            return strcmp($a->getRefId(), $b->getRefId());
-        });
-        $count = 0;
-        /**
-         * @var AdTagInterface $tag
-         */
-        foreach($adTags as $index => $tag) {
-            $librarySlotTag = $libSlotTags[$index];
-            $tag->setLibraryAdTag($librarySlotTag->getLibraryAdTag());
-            $tag->setFrequencyCap($librarySlotTag->getFrequencyCap());
-            $tag->setPosition($librarySlotTag->getPosition());
-            $tag->setRotation($librarySlotTag->getRotation());
-            $tag->setActive($librarySlotTag->isActive());
-            $tag->setImpressionCap($librarySlotTag->getImpressionCap());
-            $tag->setNetworkOpportunityCap($librarySlotTag->getNetworkOpportunityCap());
-            $em->merge($tag);
-            $count++;
-            if ($count % 100 == 0) {
-                $em->flush();
+            // remove old ad tags
+            $adTags = $adSlot->getAdTags();
+            foreach ($adTags as $t) {
+                $em->remove($t);
             }
-        }
+            $adSlot->getAdTags()->clear();
 
-        $em->flush();
+            // add new ad tags
+            /** @var LibrarySlotTagInterface $librarySlotTag */
+            foreach ($librarySlotTags as $librarySlotTag) {
+                $newAdTag = new AdTag();
+                $newAdTag->setAdSlot($adSlot)
+                    ->setRefId($librarySlotTag->getRefId())
+                    ->setLibraryAdTag($librarySlotTag->getLibraryAdTag())
+                    ->setFrequencyCap($librarySlotTag->getFrequencyCap())
+                    ->setPosition($librarySlotTag->getPosition())
+                    ->setRotation($librarySlotTag->getRotation())
+                    ->setActive($librarySlotTag->isActive())
+                    ->setImpressionCap($librarySlotTag->getImpressionCap())
+                    ->setNetworkOpportunityCap($librarySlotTag->getNetworkOpportunityCap())
+                ;
+                $adSlot->getAdTags()->add($newAdTag);
+            }
+
+            $em->persist($adSlot);
+            $em->flush();
+
+            $em->getConnection()->commit();
+        } catch (\Exception $ex) {
+            $em->getConnection()->rollback();
+            throw new RuntimeException($ex);
+        }
     }
 }
