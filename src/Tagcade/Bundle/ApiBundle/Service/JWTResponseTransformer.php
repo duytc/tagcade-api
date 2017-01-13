86be1f801a80cdc1ac53e0b544fd6d9e78f1b2eb
hieutran <dtag.dev1@gmail.com>
2017-01-13 15:53:39 +0700

    return server time for ui

--- src/Tagcade/Bundle/ApiBundle/Service/JWTResponseTransformer.php
+++ src/Tagcade/Bundle/ApiBundle/Service/JWTResponseTransformer.php
@@ -5,6 +5,7 @@ namespace Tagcade\Bundle\ApiBundle\Service;
 use Tagcade\Model\User\Role\PublisherInterface;
 use Tagcade\Model\User\Role\SubPublisherInterface;
 use Tagcade\Model\User\UserEntityInterface;
+use \DateTime;
 
 class JWTResponseTransformer
 {
@@ -14,6 +15,7 @@ class JWTResponseTransformer
         $data['username'] = $user->getUsername();
         $data['userRoles'] = $user->getUserRoles();
         $data['enabledModules'] = $user->getEnabledModules();
+        $data['serverTime'] = (new DateTime())->format('Y-m-d');
 
         if ($user instanceof PublisherInterface && !$user instanceof SubPublisherInterface) {
             $data['settings'] = $user->getSettings();
